a9ee667dade4daf343acb2d31390573423401939
parthivFarazi <parthivfarazi@icloud.com>
2025-02-06 20:55:45 -0500

    Added Forget password template

--- MoviesSite/settings.py
+++ MoviesSite/settings.py
@@ -131,3 +131,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / 'moviessite/static/',
 ]
+
+
+# Email Configuration for Password Reset
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'parthivfarazi@gmail.com'
+EMAIL_HOST_PASSWORD = 'pleaseForgiveMe'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
