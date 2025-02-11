2e4eab081fa1cda84ba9040b879a16aac7887ee4
Cashmin <Cashmin2b@gmail.com>
2025-02-11 00:03:40 -0500

    Orders and Items added.

---
+++ gt-movies-site-main/home/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from movies.models import Movie
+# Create your views here.
+def index(request):
+    search_term = request.GET.get('search')
+    if search_term:
+        movies = Movie.objects.filter(name__icontains=search_term)
+    else:
+        movies = Movie.objects.all()
+    template_data = {}
+    template_data['title'] = 'Movies Site'
+    template_data['movies'] = movies
+    return render(request, 'home/index.html', {
+        'template_data': template_data})
+def about(request):
+    template_data = {}
+    template_data['title'] = 'About'
+    return render(request, 'home/about.html',
+                  {'template_data': template_data})
