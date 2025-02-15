2089a2c0e8caccb6fea711b5d8eb00dcd715371f
Jhicks77 <jhicks77@gatech.edu>
2025-02-15 03:18:32 -0500

    Fix a search edge case, Make many style improvements

--- movies/views.py
+++ movies/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Movie, Review
 from .utils import add_movies_to_database
 
-# Create your views here.
 
 def index(request):
     search_term = request.GET.get('search')
@@ -21,6 +20,8 @@ def index(request):
     template_data['movies'] = movies
     return render(request, 'movies/index.html',
                   {'template_data': template_data})
+
+
 @login_required
 def create_review(request, id):
     if request.method == 'POST' and request.POST['comment']!= '':
@@ -33,8 +34,6 @@ def create_review(request, id):
         return redirect('movies.show', id=id)
     else:
         return redirect('movies.show', id=id)
-#def show(request, id):
-    #return HttpResponse(f'placeholder view for movie number {id}')
 
 
 def show(request, id):
@@ -52,18 +51,23 @@ def edit_review(request, id, review_id):
     review = get_object_or_404(Review, id=review_id)
     if request.user != review.user:
         return redirect('movies.show', id=id)
+
     if request.method == 'GET':
         template_data = {}
         template_data['title'] = 'Edit Review'
         template_data['review'] = review
         return render(request, 'movies/edit_review.html',{'template_data': template_data})
+
     elif request.method == 'POST' and request.POST['comment'] != '':
         review = Review.objects.get(id=review_id)
         review.comment = request.POST['comment']
         review.save()
         return redirect('movies.show', id=id)
+
     else:
         return redirect('movies.show', id=id)
+
+
 @login_required
 def delete_review(request, id, review_id):
     review = get_object_or_404(Review, id=review_id, user=request.user)
